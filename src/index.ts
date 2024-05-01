cca49a1d2086404c2a9fed358aa075aadb74bdeb
Aleksei Lapygin <alex.lapygin@gmail.com>
2024-05-01 17:57:15 +0200

    graceful shutdown

--- src/index.ts
+++ src/index.ts
@@ -8,6 +8,7 @@ import options from './config/orm.config.js';
 import { RequestContext } from '@mikro-orm/core';
 import * as dotenv from 'dotenv';
 import { userRouter } from './routers/user.router.js';
+import { Socket } from 'node:net';
 
 dotenv.config();
 
@@ -31,6 +32,43 @@ app.use((req, res, next) => {
 
 app.use(errorHandler);
 
-app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
+
+let connections: Socket[] = [];
+
+server.on('connection', (connection) => {
+  connections.push(connection);
+
+  connection.on('close', () => {
+    connections = connections.filter(
+      (currentConnection) => currentConnection !== connection
+    );
+  });
+});
+
+function shutdown() {
+  console.log('Received kill signal, shutting down gracefully');
+
+  server.close(() => {
+    console.log('Closed out remaining connections');
+    process.exit(0);
+  });
+
+  setTimeout(() => {
+    console.error(
+      'Could not close connections in time, forcefully shutting down'
+    );
+    process.exit(1);
+  }, Number(process.env.SERVER_SHUTDOWN_TIMEOUT) || 20000);
+
+  connections.forEach((connection) => connection.end());
+
+  setTimeout(() => {
+    connections.forEach((connection) => connection.destroy());
+  }, Number(process.env.CONNECTION_CLOSE_TIMEOUT) || 10000);
+}
+
+process.on('SIGTERM', shutdown);
+process.on('SIGINT', shutdown);
